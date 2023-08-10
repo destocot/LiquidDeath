367d9c14209a88bc708b0f87ed27c9ce8b03c84a
Khurram Ali <khurramcali@gmail.com>
2023-08-10 15:16:10 -0400

    search bar template created

---
+++ client/src/components/Questions-Answers/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div className="search-container">
+      <input type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS?" />
+      <i className="fa-solid fa-magnifying-glass" />
+    </div>
+  );
+}
+
+export default Search;
--- client/src/components/QuestionsAndAnswers.tsx
+++ client/src/components/QuestionsAndAnswers.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './QuestionsAndAnswers.css';
+import Search from './Questions-Answers/Search';
 
 function QuestionsAndAnswers() {
   return (
     <div className="q-and-a-container">
       <div>QUESTIONS & ANSWERS</div>
-      <div>SEARCH BAR COMPONENT</div>
+      <Search />
       <div>QUESTIONS AND ANSWERS COMPONENT</div>
       <div>EXPAND QUESTIONS / ADD QUESTIONS COMPONENT</div>
     </div>
