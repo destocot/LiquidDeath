5b95a2ad33cbd414092fe29b2cf4df03898fad42
Kurt Vardeman <kurtvardeman@gmail.com>
2023-08-10 15:22:06 -0400

    test - added ReviewsRatings component to app

---
+++ client/src/components/Reviews-Ratings/ReviewsRatings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ReviewsRatings.css';
+
+function ReviewsRatings() {
+  console.log('hello world');
+  return (
+    <div className="ratingsreviews-container">
+      Hello World from Reviews&Ratings!
+    </div>
+  );
+}
+
+export default ReviewsRatings;
