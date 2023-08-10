0466575b8a2a37bf767c882f5b3c3412a9643360
benjamin <bnt922@gmail.com>
2023-08-10 19:02:26 -0400

    Build functional review stars

--- client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
+++ client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 
 function StarsAndReviews({reviews}) {
-  function reviewAvgs({reviews}) {
-    let total = 0;
-    reviews.results.forEach((review) => {
-      total += review.rating;
-    });
-    const average = total / reviews.results.length;
-    return average;
-  }
-  // if u get 3.8, should be 3 stars and 80% of last star
-  const stars = () => {
-    const string = 'hello';
-    return string;
-  };
   // finds average of reviews
   function reviewAvg() {
     let total = 0;
@@ -21,24 +8,29 @@ function StarsAndReviews({reviews}) {
       total += review.rating;
     });
     const average = total / reviews.results.length;
+    console.log(average);
     return average;
   }
   // creates stars
-  const reviewStars = () => {
-    const average = reviewAvg();
+  const reviewStars = (num) => {
     const stars = [];
-    for (let i = 0; i < Math.floor(average); i++) {
-      stars.push(<i className="star fa-regular fa-star" />);
-    }
-    if (average - Math.floor(average) > 0) {
-      const percent = (average - Math.floor(average)) * 100;
-      stars.push(<i className="star fa-regular fa-star" style={{ width: percent }} />);
+    for (let i = 0; i < 5; i++) {
+      if (i < Math.floor(num)) {
+        stars.push(<i className="star fa-regular fa-star" />);
+      } else if (i - Math.floor(num) < 1) {
+        const percent = (num - Math.floor(num)) * 100;
+        stars.push(<i className="star fa-regular fa-star" style={{ width: percent }} />);
+      } else {
+        stars.push(<i className="empty-star fa-regular fa-star" />);
+      }
     }
     return stars;
   };
   return (
     <div>
-      {reviewStars()}
+      <div className="stars">
+        {reviewStars(reviewAvg())}
+      </div>
       <h4>Read all reviews</h4>
     </div>
   );
