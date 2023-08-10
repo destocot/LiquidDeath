74c01e7086b508a0edaaa26b8eea94119fef77c0
benjamin <bnt922@gmail.com>
2023-08-10 17:33:31 -0400

    Build functional review stars

--- client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
+++ client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function StarsAndReviews({reviews}) {
+  // finds average of reviews
   function reviewAvg() {
     let total = 0;
     reviews.results.forEach((review) => {
@@ -9,15 +10,23 @@ function StarsAndReviews({reviews}) {
     const average = total / reviews.results.length;
     return average;
   }
-  // if u get 3.8, should be 3 stars and 80% of last star
-  const stars = () => {
-    const string = 'hello';
-    return string;
+  // creates stars
+  const reviewStars = () => {
+    const average = reviewAvg();
+    const stars = [];
+    for (let i = 0; i < Math.floor(average); i++) {
+      stars.push(<i className="star fa-regular fa-star" />);
+    }
+    if (average - Math.floor(average) > 0) {
+      const percent = (average - Math.floor(average)) * 100;
+      stars.push(<i className="star fa-regular fa-star" style={{ width: percent }} />);
+    }
+    return stars;
   };
   return (
     <div>
-      <i className="star fa-regular fa-star" />
-      <h4>1 1 1 0.75 0 Read all reviews</h4>
+      {reviewStars()}
+      <h4>Read all reviews</h4>
     </div>
   );
 }
