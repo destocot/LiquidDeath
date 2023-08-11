5e94f9aa31bf5d822367bb9d399e74ca8657895f
Kurt Vardeman <kurtvardeman@gmail.com>
2023-08-11 11:56:48 -0400

    added average rating using reviews metadata

--- client/src/components/Reviews-Ratings/RatingBreakdown.jsx
+++ client/src/components/Reviews-Ratings/RatingBreakdown.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
-function RatingBreakdown() {
+import { getReviewsMeta } from './exampleData.js'; // putting this here to until master updated with prev changes
+
+function RatingBreakdown(/* props when getReviewsMeta is moved */) {
+  // data comes from getReviewsMeta
+  const calcAvgRating = (ratingsObj) => {
+    if (Object.keys(ratingsObj).length > 0) {
+      let sumOfRatings = 0;
+      let product = 0;
+      for (var key in ratingsObj) {
+        let rating = key;
+        let numberOfRatings = ratingsObj[key];
+        product += rating * numberOfRatings;
+        sumOfRatings += numberOfRatings;
+      }
+      let avgRating = product / sumOfRatings;
+      avgRating = Math.round(avgRating * 10) / 10;
+      return avgRating;
+    }
+  };
+
+  // console.log(getReviewsMeta.ratings);
+  // const avgRating = calcAvgRating(getReviewsMeta.ratings);
+  // console.log({avgRating});
+
+  // ben's star functionality goes here
+
   return (
     <div className="ratingBreakdown">
       <h2>Rating Breakdown</h2>
-      <div>Rating Summary</div>
+      <div>{calcAvgRating(getReviewsMeta.ratings)} ⭐⭐⭐⭐</div>
       <div>Breakdown</div>
       <div>Filters Applied</div>
       <div>Reset Filters</div>
