7812b7e6c26a1a90ee91317e3b895cb86bee093a
Kurt Vardeman <kurtvardeman@gmail.com>
2023-08-11 20:39:54 -0400

    issues w/ list not rerendering when sort component sorts array and passes back up to parent.

--- client/src/components/Reviews-Ratings/ReviewsList.jsx
+++ client/src/components/Reviews-Ratings/ReviewsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReviewTile from './ReviewTile';
 import Sorting from './Sorting';
 
@@ -15,10 +15,20 @@ function ReviewsList({ filteredReviews, filters }) {
 >>>>>>> 4cc3b44 (filters from breakdown affect list rendering now)
   let moreButton;
   const [listLength, setListLength] = useState(2);
+  const [sortedFilteredReviews, setSortedFilteredReviews] = useState(filteredReviews);
+
+  const updateSetSortedFilteredReviews = (sortedArray) => {
+    console.log(sortedArray);
+    setSortedFilteredReviews(sortedArray);
+  };
 
   // renders 2 review tiles at a time using .slice and the listLength state
-  const reviewTile = filteredReviews.map((review) =>
-    <div key={review.review_id}><ReviewTile review={review}/></div>).slice(0, listLength);
+  const reviewTile = () => {
+    console.log('rerunning reviewTile');
+    console.log(sortedFilteredReviews);
+    return sortedFilteredReviews.map((review) =>
+    <div key={review.review_id}><ReviewTile review={review} /></div>).slice(0, listLength);
+  };
 
   // increases listLength by 2, in turn rendering two more elements
   const handleClick = () => {
@@ -27,16 +37,17 @@ function ReviewsList({ filteredReviews, filters }) {
   };
 
   // decides whether or not button should be rendered based on length of results
-  if (filteredReviews.length > 2) {
+  if (sortedFilteredReviews.length > 2) {
     moreButton = <button className="moreReviewsButton" type="button" onClick={handleClick}>More Reviews</button>;
   } else {
     moreButton = <div></div>;
   }
+
   return (
     <div className="reviewsList">
       <h2>Reviews List</h2>
-      <Sorting filteredReviews={filteredReviews} />
-      {reviewTile}
+      <Sorting sortedFilteredReviews={sortedFilteredReviews} updateSetSortedFilteredReviews={updateSetSortedFilteredReviews} />
+      {reviewTile()}
       {/* <div>Individual Review Tile</div> */}
       {moreButton}
       {/* <div>Add Review</div> */}
--- client/src/components/Reviews-Ratings/Sorting.jsx
+++ client/src/components/Reviews-Ratings/Sorting.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 
-function Sorting({ filteredReviews }) {
+function Sorting({ sortedFilteredReviews, updateSetSortedFilteredReviews }) {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    const tempArray = sortedFilteredReviews;
+
+    tempArray.sort((a, b) => {
+      const revA = a.helpfulness;
+      const revB = b.helpfulness;
+      if (revA < revB) {
+        return 1;
+      }
+      if (revA > revB) {
+        return -1;
+      }
+      return 0;
+    });
+    updateSetSortedFilteredReviews(tempArray);
+  };
+
   return (
     <div>
       <label for="sortFilter">Sort Options:</label>
-      <select name="sortFilter">
+      <select name="sortFilter" onChange={handleClick}>
         <option value="relevant">Relevant</option>
         <option value="helpful">Helpful</option>
         <option value="newest">Newest</option>
