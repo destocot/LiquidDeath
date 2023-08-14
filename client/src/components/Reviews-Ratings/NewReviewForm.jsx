a3b3183c12ef978200ee62ad723fe7159dea8d90
Kurt Vardeman <kurtvardeman@gmail.com>
2023-08-14 16:49:22 -0400

    added comments to new module before pulling and rebasing from master

--- client/src/components/Reviews-Ratings/NewReviewForm.jsx
+++ client/src/components/Reviews-Ratings/NewReviewForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 function NewReviewForm({ setAForm, getReviewsMeta }) {
   const [rating, setRating] = useState(0);
-  // const [hover, setHover] = useState(0);
   const [recommendation, setRecommendation] = useState(false);
   const [characteristics, setCharacteristics] = useState({Size: null, Width: null, Comfort: null, Quality: null, Length: null, Fit: null});
 
@@ -14,16 +13,19 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
     5: "Great"
   };
 
+  // used to update the boolean recommend
   const updateRecommendation = (value) => {
     setRecommendation(value);
   };
 
+  // update characteristics
   const updateCharacteristics = (key, value) => {
     const newCharacteristics = { ...characteristics, [key]: value };
     ('updated: ', newCharacteristics);
     setCharacteristics(newCharacteristics);
   };
 
+  // this renders 5 stars that allow user to choose, upon choosing, renders gold stars up to and including what user has selected, fills rest in with grey
   const renderStars = () => {
     return [1, 2, 3, 4, 5].map((index, value) => {
       if (index <= rating) {
@@ -38,6 +40,7 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
     })
   };
 
+  // generates radio buttons for users to rank characteristics
   const renderCharacteristics = () => {
     return Object.keys(getReviewsMeta.characteristics).map((characteristic) => {
       return (
@@ -56,23 +59,31 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
     })
   };
 
+  // TODO - update this to generate popup window where user can add photos
   const renderPhotoPage = () => {
     ('click');
   };
 
+  // Part of Khurram's code
   const checkKeyDown = (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
     }
-    // if (e.key === 'Escape') {
-    //   e.preventDefault();
-    //   close();
-    // }
+    if (e.key === 'Escape') {
+      e.preventDefault();
+      close();
+    }
   };
 
+  // Part of Khurram's code
+  const close = () => {
+    setForm(false);
+  };
+
+  // TODO - update this to store all values in a massive state
   const submitHandler = (e) => {
     e.preventDefault();
-    ('submitHandler clicked');
+    console.log('submitHandler clicked');
     // const body = e.target.body.value;
     // const name = e.target.name.value;
     // const email = e.target.email.value;
@@ -82,14 +93,11 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
     // ({ body, name, email, product_id });
   };
 
+  // TODO - update this to post to reviews
   // const sendQuestion = (data) => {
   //   axios.post('/qa/questions', data)
   //     .catch(() => ('error posting question'));
   // }
-
-  const close = () => {
-    setForm(false);
-  };
 
   return (
     <div>
