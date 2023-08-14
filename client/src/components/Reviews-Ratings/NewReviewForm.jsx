ef23547ad9248c48aa0e4422bb76771176defcf8
Kurt Vardeman <kurtvardeman@gmail.com>
2023-08-14 18:25:28 -0400

    when characteristic review is selected, that value appears above radio buttons

--- client/src/components/Reviews-Ratings/NewReviewForm.jsx
+++ client/src/components/Reviews-Ratings/NewReviewForm.jsx
@@ -1,17 +1,10 @@
 import React, { useState } from 'react';
+import { characteristicLabels, starMeaning } from './helpers';
 
 function NewReviewForm({ setAForm, getReviewsMeta }) {
   const [rating, setRating] = useState(0);
   const [recommendation, setRecommendation] = useState(false);
   const [characteristics, setCharacteristics] = useState({Size: null, Width: null, Comfort: null, Quality: null, Length: null, Fit: null});
-
-  const starMeaning = {
-    1: "Poor",
-    2: "Fair",
-    3: "Average",
-    4: "Good",
-    5: "Great"
-  };
 
   // used to update the boolean recommend
   const updateRecommendation = (value) => {
@@ -43,17 +36,29 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
   // generates radio buttons for users to rank characteristics
   const renderCharacteristics = () => {
     return Object.keys(getReviewsMeta.characteristics).map((characteristic) => {
+      // characteristic refers to Size, Width, Comfort, etc.
+      // characteristics refers to the state, an object with properties for each characteristic
+      const currentCharValue = characteristics[characteristic] // could be null or a number
+
       return (
-        <div style={{ display:'flex' }}>{characteristic}
-          {
-            [1, 2, 3, 4, 5].map((index) => {
-              return (
-                <label>
-                  <input type="radio" name={characteristic} value={index} checked={characteristics[characteristic] === index} onChange={() => updateCharacteristics(characteristic, index)}/>
-                </label>
-              )
-            })
-          }
+        <div style={{ display:'flex', flexDirection: 'column' }}>{characteristic} <br />
+          {!currentCharValue ? <div>none selected</div> : <div>{characteristicLabels[characteristic][currentCharValue]}</div>}
+          <div>
+            {
+              [1, 2, 3, 4, 5].map((index) => {
+                return (
+                    <label>
+                      <input type="radio" name={characteristic} value={index} checked={characteristics[characteristic] === index} onChange={() => updateCharacteristics(characteristic, index)}/>
+                    </label>
+                )
+              })
+            }
+          </div>
+          <div style={{ display:'flex'}}className="characteristicsAxisLabels">
+            {<div>{characteristicLabels[characteristic]['1']}</div>}
+            {<div>{characteristicLabels[characteristic]['5']}</div>}
+          </div>
+
         </div>
       )
     })
@@ -107,32 +112,32 @@ function NewReviewForm({ setAForm, getReviewsMeta }) {
       </div>
       <form onSubmit={(e) => submitHandler(e)} onKeyDown={(e) => checkKeyDown(e)}>
         {/* Overall Rating by Clicking Number of Stars */}
-        <label>Overall Rating<br />
+        <label required>Overall Rating<br />
           <div className="newReviewStarRating">
             {renderStars()}
             {(rating !== 0) ? <div>{starMeaning[rating]}</div> : null}
           </div>
         </label>
         {/* Boolean Product Recommendation - utilizes radio buttons */}
-        <label>Do you recommend this product?<br />
+        <label required>Do you recommend this product?<br />
           <label>Yes
             <input type="radio" name="recommendation_true" value={true} checked={recommendation} onChange={() => updateRecommendation(true)}/>
           </label>
           <label>No
-            <input type="radio" name="recommendation_false" value={false} checked={!recommendation} onChange={() => updateRecommendation(false)} />
+            <input type="radio" name="recommendation_false" value={false} checked={!recommendation}  onChange={() => updateRecommendation(false)} />
           <br /></label>
         </label>
         {/* Characteristics */}
-        <label className="newReviewCharacteristics">Characteristics <br />
+        <label className="newReviewCharacteristics" required>Characteristics <br />
           <div>{renderCharacteristics()}</div>
         </label>
         {/* Text Inputs */}
         <label>Review Summary <br />
           <textarea maxLength="100" name="summary" /> <br /></label>
         <label>Review Body <br />
-          <textarea maxLength="1000" rows="5" name="body" /> <br /></label>
+          <textarea maxLength="1000" rows="5" name="body" required /> <br /></label>
         <label>Nickname<br />
-          <input type="text" name="nickname" placeholder="happycustomer20" /><br /></label>
+          <input type="text" name="nickname" placeholder="happycustomer20" required /><br /></label>
         <label >E-mail<br />
           <input type="email" maxLength="60" placeholder="jack@email.com" name="email" required /> <br /> </label>
         <button onClick={renderPhotoPage}>Add Photos  </button><br />
