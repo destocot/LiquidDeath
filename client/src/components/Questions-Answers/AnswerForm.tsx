fc0f289b0b0b668e995e9d24506ae732e28d7962
destocot <khurramcali@gmail.com>
2023-08-14 21:16:05 -0400

    image uploads working

--- client/src/components/Questions-Answers/AnswerForm.tsx
+++ client/src/components/Questions-Answers/AnswerForm.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FormStyles.css';
 import axios from 'axios';
 
 function AnswerForm({ setAForm, currProductName, questionBody, questionId }) {
+  const [myPhotos, setMyPhotos] = useState([]);
 
   // prevents form from being submitted on enter
   const checkKeyDown = (e) => {
@@ -15,21 +16,50 @@ function AnswerForm({ setAForm, currProductName, questionBody, questionId }) {
     }
   };
 
+  const [uploadNum, setUploadNum] = useState(0);
+  const imageChecker = (e) => {
+    const files = e.target.files;
+    setUploadNum(files.length);
+    let imageDiv = document.getElementById('my-images-div');
+    let imageInput = document.getElementById('ans-photos');
+    imageDiv.innerHTML = '';
+
+    if (files.length > 5) {
+      alert('You can only upload up to 5 photos!');
+      imageInput.value = '';
+    } else {
+      for (var i = 0; i < files.length; i++) {
+        var reader = new FileReader();
+        reader.addEventListener("load", (event) => {
+          const picFile = event.target;
+          const div = document.createElement('div');
+          div.innerHTML = `<img className="thumbnail" src="${picFile.result}" alt="${picFile.name}" />`;
+          imageDiv?.appendChild(div);
+        })
+        reader.readAsDataURL(files[i])
+      }
+    }
+  }
+
   const submitHandler = (e) => {
     e.preventDefault();
-    const body = e.target.body.value;
-    const name = e.target.name.value;
-    const email = e.target.email.value;
-    const question_id = questionId;
-    sendAnswer(question_id, { body, name, email });
-    // console.log({ body, name, email, question_id });
-    close();
+    if (uploadNum > 5) {
+      alert('You can only upload up to 5 photos!');
+    } else {
+      const form = document.getElementById('answer-form');
+      const formData = new FormData(form);
+      sendAnswer(questionId, formData);
+      close();
+    }
   }
 
   const sendAnswer = (question_id, data) => {
-    // console.log(question_id);
-    axios.post(`/qa/questions/${question_id}/answers`, data)
-      .catch(() => console.log('error posting question'));
+    axios.post(`/qa/questions/${question_id}/answers`, data, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+      .catch(() => console.log('error posting answer', data));
   }
 
   const close = () => {
@@ -45,17 +75,18 @@ function AnswerForm({ setAForm, currProductName, questionBody, questionId }) {
         </div>
         <h3>{currProductName}:</h3>
         <h3>{questionBody}</h3>
-        <form id="answer-form" onSubmit={(e) => submitHandler(e)} onKeyDown={(e) => checkKeyDown(e)}>
+        <form id="answer-form" onSubmit={(e) => submitHandler(e)} onKeyDown={(e) => checkKeyDown(e)} encType="multipart/form-data">
           <label>Answer<br />
-            <textarea maxLength="1000" rows="3" name="body" required /></label>
+            <textarea maxLength="1000" rows="2" name="body" required /></label>
           <label>Name<br />
             <input type="text" maxLength="60" placeholder="Example: jackson543!" name="name" required /></label>
-            <h6>For privacy reasons, do not use your full name or email address</h6>
+          <h6>For privacy reasons, do not use your full name or email address</h6>
           <label >E-mail<br />
             <input type="email" maxLength="60" placeholder="jack@email.com" name="email" required /></label>
-            <h6>For authentication reasons, you will not be emailed</h6>
+          <h6>For authentication reasons, you will not be emailed</h6>
           <label >Upload your photos<br />
-            <input type="file" name="photos" disabled /></label>
+            <input id="ans-photos" type="file" name="photos" accept="image/png, image/jpeg" onChange={(e) => imageChecker(e)} multiple /></label>
+          <div id="my-images-div"></div>
           <input id="a-submit-btn" type="submit" />
         </form>
       </div>
