b1b8e909ec73eecae2fc1a0f2af78d141fc3bd7c
benjamin <bnt922@gmail.com>
2023-08-16 17:49:46 -0400

    Check mark overlay progress

--- client/src/components/Overview/Features/StyleSelector/index.tsx
+++ client/src/components/Overview/Features/StyleSelector/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import helpers from '../../../../helpPlease';
 
 function StyleSelector({styles, currentStyle, setCurrentStyle}) {
+      // creates matrix with style rows
+      const styleMatrix = () => {
+        let row = [];
+        let matrix = [];
+        styles.forEach((style, i) => {
+          row.push(
+          <li key={style.style_id} className="style-list-item">
+            <div className="style" >
+              <img
+                id={i}
+                alt={style.name}
+                className="style-thumbnail"
+                src={style.photos[0].thumbnail_url}
+                onClick={setCurrentStyle}
+              />
+              <div className="style-overlay">
+                <i className="fa-solid fa-lg fa-circle-check"></i>
+              </div>
+            </div>
+          </li>
+          );
+          if (row.length === 4) {
+            matrix.push(<ul key={matrix.length} className="style-row">{row}</ul>)
+            matrix.push(<div key={`break${matrix.length}`} className="break"></div>)
+            row = [];
+          }
+        });
+        return matrix;
+      },
   return (
     <div>
       <h2 className="style-title">{currentStyle.name}</h2>
       <div className="style-selector-container">
-        {helpers.styleMatrix(styles, setCurrentStyle)}
+        {styleMatrix()}
       </div>
     </div>
   )
