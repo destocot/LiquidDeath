41418716c8759667f7fb191c8e0cc37afe030e2d
benjamin <bnt922@gmail.com>
2023-08-16 20:42:14 -0400

    Functional overlay check mark

--- client/src/components/Overview/Features/StyleSelector/index.tsx
+++ client/src/components/Overview/Features/StyleSelector/index.tsx
@@ -1,35 +1,42 @@
 import React from 'react';
 
 function StyleSelector({styles, currentStyle, setCurrentStyle}) {
-      // creates matrix with style rows
-      const styleMatrix = () => {
-        let row = [];
-        let matrix = [];
-        styles.forEach((style, i) => {
-          row.push(
-          <li key={style.style_id} className="style-list-item">
-            <div className="style" >
-              <img
-                id={i}
-                alt={style.name}
-                className="style-thumbnail"
-                src={style.photos[0].thumbnail_url}
-                onClick={setCurrentStyle}
-              />
-              <div className="style-overlay">
-                <i className="fa-solid fa-lg fa-circle-check"></i>
-              </div>
-            </div>
-          </li>
-          );
-          if (row.length === 4) {
-            matrix.push(<ul key={matrix.length} className="style-row">{row}</ul>)
-            matrix.push(<div key={`break${matrix.length}`} className="break"></div>)
-            row = [];
-          }
-        });
-        return matrix;
-      },
+  // creates overlayed checkmark
+  const selected = () => (
+    <div className="fa-stack">
+      <i className="solid-check fa-solid fa-lg fa-circle-check fa-stack-1x" />
+      <i className="regular-check fa-regular fa-lg fa-circle-check fa-stack-1x" />
+      {/* <i className="fa-solid fa-lg fa-circle-check"></i> */}
+    </div>
+  );
+  // creates matrix with style rows
+  const styleMatrix = () => {
+    let row = [];
+    let matrix = [];
+    styles.forEach((style, i) => {
+      row.push(
+      <li key={style.style_id} className="style-list-item">
+        <div className="style" >
+          <img
+            id={i}
+            alt={style.name}
+            className="style-thumbnail"
+            src={style.photos[0].thumbnail_url}
+            onClick={setCurrentStyle}
+          />
+          {currentStyle.name === style.name ? selected() : null}
+        </div>
+      </li>
+      );
+      if (row.length === 4) {
+        matrix.push(<ul key={matrix.length} className="style-row">{row}</ul>)
+        matrix.push(<div key={`break${matrix.length}`} className="break"></div>)
+        row = [];
+      }
+    });
+    return matrix;
+  };
+
   return (
     <div>
       <h2 className="style-title">{currentStyle.name}</h2>
