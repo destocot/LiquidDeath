7d677226a169994a15f8cf76001b41e82efb234e
destocot <khurramcali@gmail.com>
2023-08-17 14:04:17 -0400

    redoing question expansio

--- client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.tsx
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.tsx
@@ -9,21 +9,25 @@ function QuestionsAndAnswers({ currProductId, currProductName }) {
   const [numOfQuestions, setNumOfQuestions] = useState(2);
   const [query, setQuery] = useState('');
 
+  const [triggor, setTriggor] = useState(false);
+
   return (
     <div className="qa-main-container">
       <h2 id="qa-main-title" className="h-[5vh] text-[2rem]">QUESTIONS & ANSWERS</h2>
       <Search setQuery={setQuery} />
       <QuestionList
-      setDisplayMore={setDisplayMore}
-      numOfQuestions={numOfQuestions}
-      query={query}
-      currProductName={currProductName}
-      currProductId={currProductId} />
+        setDisplayMore={setDisplayMore}
+        numOfQuestions={numOfQuestions}
+        query={query}
+        currProductName={currProductName}
+        currProductId={currProductId}
+        triggor={triggor} />
       <ExpandAndAdd
-      displayMore={displayMore}
-      setNumOfQuestions={setNumOfQuestions}
-      currProductName={currProductName}
-      currProductId={currProductId}
+        displayMore={displayMore}
+        setNumOfQuestions={setNumOfQuestions}
+        currProductName={currProductName}
+        currProductId={currProductId}
+        setTriggor={setTriggor}
       />
     </div>
   );
