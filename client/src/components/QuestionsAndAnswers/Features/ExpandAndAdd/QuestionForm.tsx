87b46dcc399d23c416729ea5ba915722dbc5e683
destocot <khurramcali@gmail.com>
2023-08-17 13:28:43 -0400

    liquid death presents worlds cleanest form

---
+++ client/src/components/QuestionsAndAnswers/Features/ExpandAndAdd/QuestionForm.tsx
@@ -0,0 +1,90 @@
+// @ts-nocheck
+import React from 'react';
+import axios from 'axios';
+
+//POPUP TEST
+import Popup from 'reactjs-popup';
+import './Popup.css';
+// import 'reactjs-popup/dist/index.css';
+
+function QuestionForm({ currProductName, currProductId }) {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const body = e.target.body.value;
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const product_id = currProductId;
+    sendQuestion({ body, name, email, product_id });
+    console.log({ body, name, email, product_id });
+  }
+
+  // onClick={() => {
+  //   document.body.style.overflow = 'hidden';
+  //   setQForm(true)
+  // }
+
+  const sendQuestion = (data) => {
+    axios.post('/qa/questions', data)
+      .catch(() => console.log('error posting question'));
+  }
+  return (
+    <Popup Popup trigger={<button button id="add-questions-btn" > ADD A QUESTION < i className="fa-solid fa-plus" /> </button >} modal >
+      {(close) => (
+        <div className="popup-modal-container">
+          <div className="modal">
+            <div className="flex flex-col mb-3">
+              <div className="flex justify-between items-center relative">
+                <div className="text-[1.5em]">Ask Your Question</div>
+                <i
+                  className="fa-regular fa-circle-xmark fa-2xl cursor-pointer absolute z-2 top-[-20px] right-[-20px] bg-white leading-5 text-[24px] hover:leading-6 hover:text-[32px]"
+                  onClick={close}
+                />
+              </div>
+              <div className="text-[1.17em]">About the {currProductName}</div>
+            </div>
+
+            <form
+              className="flex flex-col justify-around h-[40vh] text-base p-2"
+              onSubmit={(e) => { submitHandler(e); close(); }}>
+              <label>Your Question*<br />
+                <textarea
+                  className="w-full border-2 p-1"
+                  maxLength="1000"
+                  rows="3"
+                  name="body"
+                  required /></label>
+              <div>
+                <label>Name*<br />
+                  <input
+                    className="w-full border-2 p-1"
+                    type="text"
+                    maxLength="60"
+                    placeholder="Example: jackson11!"
+                    name="name"
+                    required /></label>
+                <h6>For privacy reasons, do not use your full name or email address</h6>
+              </div>
+              <div>
+                <label >E-mail*<br />
+                  <input
+                    className="w-full border-2 p-1"
+                    type="email"
+                    maxLength="60"
+                    placeholder="jack@email.com"
+                    name="email"
+                    required /></label>
+                <h6>For authentication reasons, you will not be emailed</h6>
+              </div>
+              <input
+                className="border-2 hover:border-[black] cursor-pointer self-center px-6 py-[0.313rem] m-1"
+                type="submit" />
+              <button className="border hover:border-[black] self-center px-2 py-1" onClick={close}>Close</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </Popup >
+  );
+}
+
+export default QuestionForm
