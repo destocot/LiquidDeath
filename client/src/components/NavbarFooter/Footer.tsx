48b40cd23989f3bcc0f2737b7ca0fbf938be524e
destocot <khurramcali@gmail.com>
2023-08-19 11:42:51 -0400

    footer

--- client/src/components/NavbarFooter/Footer.tsx
+++ client/src/components/NavbarFooter/Footer.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="h-fit py-[0.5vh] mt-[2vh] border-t">
-      <p>Author: Hege Refsnes</p>
-      <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+    <footer className="h-fit mt-[2vh] border-t">
+      <div className="flex justify-between w-[20vw] mx-auto py-[5px]">
+        <span>BT</span>
+        <span>|</span>
+        <span>JW</span>
+        <span>|</span>
+        <span>KA</span>
+        <span>|</span>
+        <span>KV</span>
+        <span></span>
+      </div>
+      <h6 className="mx-auto w-[20vw] text-center pb-[5px]">© 2023 Liquid Death, Inc.</h6>
     </footer>
   )
 }
