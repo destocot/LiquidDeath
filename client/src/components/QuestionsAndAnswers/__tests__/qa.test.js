73d9e96e9fc6d5bfce1b79e79fccc7b8874ea68c
destocot <khurramcali@gmail.com>
2023-08-21 15:27:40 -0400

    updated new test

--- client/src/components/QuestionsAndAnswers/__tests__/qa.test.js
+++ client/src/components/QuestionsAndAnswers/__tests__/qa.test.js
@@ -1,22 +1,28 @@
 import React from "react";
-import QuestionForm from "../Features/Forms/QuestionForm";
-import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import QuestionForm from "../Features/ExpandAndAdd/QuestionForm";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 const currProductId = 37324;
 const currProductName = "Jaylen Backpack";
 
 test("check text", () => {
-  render(
+  const { getByTestId } = render(
     <QuestionForm
       currProductId={currProductId}
       currProductName={currProductName}
     />
   );
 
-  const nameWarning = screen.getByText(/For privacy reasons, do not use your full name or email address/i);
+  fireEvent.click(getByTestId("add-questions-btn"));
+
+  const nameWarning = screen.getByText(
+    /For privacy reasons, do not use your full name or email address/i
+  );
   expect(nameWarning).toBeInTheDocument();
 
-  const emailWarning = screen.getByText(/For authentication reasons, you will not be emailed/i);
+  const emailWarning = screen.getByText(
+    /For authentication reasons, you will not be emailed/i
+  );
   expect(emailWarning).toBeInTheDocument();
 });
