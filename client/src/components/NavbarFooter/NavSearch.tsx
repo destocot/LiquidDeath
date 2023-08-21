06cade4a26c5734b964424f66503165be7f86997
destocot <khurramcali@gmail.com>
2023-08-21 13:56:44 -0400

    more css

--- client/src/components/NavbarFooter/NavSearch.tsx
+++ client/src/components/NavbarFooter/NavSearch.tsx
@@ -16,7 +16,7 @@ function NavSearch({ changeSearch }) {
     <div>
       <input id="nav-search-bar" className="border-2 py-1 px-2 rounded-[1.625rem] text-[black]" type="text" placeholder="Search products" onChange={(e) => navSearch(e)}></input>
       {
-        !!searchProducts.length && <div className="bg-[black]/[0.8] z-[10] absolute flex flex-col px-[2vw] mt-[0.25vh]">
+        !!searchProducts.length && <div className="bg-[black]/[0.8] z-[10] absolute flex flex-col px-[calc(1.3vw_+_0.5rem)] mt-[0.25vh]">
           {
             searchProducts.map((searchProduct) => (<span className="searchProd text-white hover:text-[#007185]" onClick={() => {
               changeSearch(searchProduct.id);
