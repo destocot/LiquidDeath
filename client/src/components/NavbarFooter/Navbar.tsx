9f34461cdb5ffd40889843ead14476dafb40ae0e
destocot <khurramcali@gmail.com>
2023-08-23 10:34:39 -0400

    final css 1

--- client/src/components/NavbarFooter/Navbar.tsx
+++ client/src/components/NavbarFooter/Navbar.tsx
@@ -40,7 +40,11 @@ function Navbar({ changeSearch, numInCart }) {
           <span className="mr-[16px]">Home</span>
           <span className="mr-[16px]">Sale</span>
           <span className="mr-[16px]">About</span>
-          <button id="color-mode-btn" className="hover:underline px-[1px] mr-[16px]" onClick={() => changeColor()}>{colorMode}</button>
+          <button id="color-mode-btn" className="hover:underline px-[1px] mr-[16px]" onClick={() => changeColor()}>
+            {(colorMode === 'Dark Mode') && <i className="fa-solid fa-moon" />}
+            {(colorMode === 'Light Mode') && <i className="fa-solid fa-sun" />}
+            {/* // {colorMode} */}
+          </button>
         </div>
       </div>
       <div className="row-start-1 row-end-2 col-start-5 col-end-7 justify-self-end">
