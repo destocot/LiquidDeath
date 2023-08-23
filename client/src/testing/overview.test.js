8e9b5e63f6e1a739e37602a8c3200fc364ca0307
benjamin <bnt922@gmail.com>
2023-08-22 21:05:33 -0400

    TESTING TESTING

--- client/src/testing/overview.test.js
+++ client/src/testing/overview.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { render, screen, cleanup, fireEvent, act } from '@testing-library/react';
 import "@testing-library/jest-dom";
-import ProductIdentifiers from '../components/Overview/Features/ProductInfo/ProductIdentifiers';
 import initial from "../PlaceHolderData.js";
 import axios from 'axios';
+// components, features, and helper functions for testing
 import Overview from '../components/Overview';
+import AddCart from '../components/Overview/Features/AddCart';
+import ImageGallery from '../components/Overview/Features/ImageGallery';
+import ProductInfo from '../components/Overview/Features/ProductInfo';
+import CartForm from '../components/Overview/Features/AddCart/CartForm';
+import BigCarousel from '../components/Overview/Features/ImageGallery/BigCarousel';
+import BigImage from '../components/Overview/Features/ImageGallery/BigImage';
+import Image from '../components/Overview/Features/ImageGallery/Image';
+import ImageCarousel from '../components/Overview/Features/ImageGallery/ImageCarousel';
+import Description from '../components/Overview/Features/ProductInfo/Description';
+import ProductIdentifiers from '../components/Overview/Features/ProductInfo/ProductIdentifiers';
+import Sharing from '../components/Overview/Features/ProductInfo/Sharing';
+import StarsAndReviews from '../components/Overview/Features/ProductInfo/StarsAndReviews';
+import helpers from '../helpPlease';
 
 jest.mock("axios");
 jest.mock("react", () => ({
@@ -85,122 +98,206 @@ describe('Overview Component Test', () => {
   })
 });
 
-/*
-const testReviewArray = [
-  {
-    helpfulness: 5,
-    date: '2023-04-14T00:00:00.000Z'
-  },
-  {
-    helpfulness: 3,
-    date: '2018-04-14T00:00:00.000Z'
-  },
-  {
-    helpfulness: 1,
-    date: '2020-04-14T00:00:00.000Z'
-  },
-];
-
-describe("sumHelper", () => {
-
-  test('sumHelper should return the sum of an array', () => {
-    expect(sumHelper([1, 2, 3])).toEqual(6);
+describe('AddCart Component Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
   });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the AddCart component', () => {
+    render(
+    <AddCart
+    currentStyle={initial.styles.results[0]}
+    setConfetti={setConfetti}
+    setNumInCart={setNumInCart}
+    />);
+  })
+});
 
-  test('sumHelper should return 0 if array is empty', () => {
-    expect(sumHelper([])).toEqual(0);
+describe('ImageGallery Component Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
   });
-
-});
-
-describe("recommended" , () => {
-  const recommended = helpers.recommended;
-  var testObj = {
-    true: '2',
-    false: '8',
-  };
-
-  test('recommended function should count true and false values and return % of true as a string', () => {
-    expect(recommended(testObj)).toBe('20%');
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the ImageGallery component', () => {
+    render(
+    <ImageGallery
+    currentStyle={initial.styles.results[0]}
+    setCurrentStyle={() => false}
+    styles={initial.styles.results}
+    view={false}
+    toggleExpand={() => false}
+    windowSize={windowSize}
+    />);
   })
 });
 
-describe("calcAvgRating" , () => {
-  const calcAvgRating = helpers.calcAvgRating;
-  var testObj = {1: '1', 2: '1', 3: '0', 4: '0', 5: '0'};
-
-  test('calcAvgRating should take an object and return a value representing average rating', () => {
-    expect(calcAvgRating(testObj)).toBe(1.5);
+describe('ProductInfo Component Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the ProductInfo component', () => {
+    render(
+    <ProductInfo
+    product={initial.product}
+    currentStyle={initial.styles.results[0]}
+    reviewsMeta={reviewsMeta}
+    setConfetti={setConfetti}
+    />);
   })
 });
 
-describe("normalizeData" , () => {
-  const normalizeData = helpers.normalizeData;
-  var testObj = [
-    {helpfulness: 1, date: "2020-08-12T00:00:00.000Z"},
-    {helpfulness: 2, date: "2023-08-12T00:00:00.000Z"},
-  ]
-
-  test('should rank items with low helpfulness and long ago dates lower than recent and high helpfulness', () => {
-    var result = (normalizeData(testObj));
-    expect(result[0].sortRelMetric).toBe(0);
-    expect(result[1].sortRelMetric).toBeGreaterThan(0);
-  })
-
-});
-
-describe("sortRelevance" , () => {
-  const sortRelevance = helpers.sortRelevance;
-  var testObj = [
-    {name: 'c', helpfulness: 1, date: "2020-08-12T00:00:00.000Z"},
-    {name: 'a', helpfulness: 3, date: "2023-08-12T00:00:00.000Z"},
-    {name: 'b', helpfulness: 2, date: "2021-08-12T00:00:00.000Z"},
-  ]
-
-  test('should alter order of the array', () => {
-    var result = (sortRelevance(testObj));
-    expect(result[0].name).toBe('a');
-    expect(result[1].name).toBe('b');
-    expect(result[2].name).toBe('c');
+describe('CartForm feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the CartForm feature', () => {
+    render(
+    <CartForm
+    currentStyle={initial.styles.results[0]}
+    handleSubmit={(e) => e.preventDefault()}
+    needed={false}
+    setNeeded={() => true}
+    size={'SELECT SIZE'}
+    setSize={() => 'small'}
+    quantity={'-'}
+    setQuantity={() => 5}
+    />);
   })
 });
 
-describe("charChecker" , () => {
-  const charChecker = helpers.charChecker;
-  var testStateObjPass = {
-    1: 3,
-    2: 2,
-  }
-
-  var testStateObjFailAll = {
-  }
-
-  var testStateObjFailPartial = {
-    1: 3,
-  }
-
-  var testProductChars = {
-    Quality: {
-      id: 1,
-      value: "not important",
-    },
-    Size: {
-      id: 2,
-      value: "not important"
-    }
-
-  }
-
-  test('should return true if user has checked a rating for all characteristics in form', () => {
-    expect(charChecker(testStateObjPass, testProductChars)).toBe(true);
-  })
-
-  test('should return false if user has not checked a rating for any characteristics in form', () => {
-    expect(charChecker(testStateObjFailAll, testProductChars)).toBe(false);
-  })
-
-  test('should return false if user has not checked a rating for all characteristics in form', () => {
-    expect(charChecker(testStateObjFailPartial, testProductChars)).toBe(false);
+describe('BigCarousel feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the BigCarousel feature', () => {
+    render(
+      <BigCarousel
+      currentStyle={initial.styles.results[0]}
+      img={initial.styles.results[0].photos[0].url}
+      setImg={() => false}
+      styles={initial.styles.results}
+      setCurrentStyle={() => false}
+    />);
   })
 });
-*/
+
+describe('BigImage feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the BigImage feature', () => {
+    render(
+      <BigImage
+      windowSize={windowSize}
+      currentStyle={initial.styles.results[0]}
+      img={initial.styles.results[0].photos[0].url}
+      setImg={() => false}
+      styles={initial.styles.results}
+      setCurrentStyle={() => false}
+    />);
+  })
+});
+
+describe('Image feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the Image feature', () => {
+    render(
+      <Image
+      toggleExpand={() => false}
+      currentStyle={initial.styles.results[0]}
+      img={initial.styles.results[0].photos[0].url}
+    />);
+  })
+});
+
+describe('ImageCarousel feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the ImageCarousel feature', () => {
+    render(
+      <ImageCarousel
+      up={false}
+      down={false}
+      toggleUp={() => false}
+      toggleDown={() => false}
+      styles={initial.styles.results}
+      setImg={() => false}
+      setCurrentStyle={() => false}
+      toggleExpand={() => false}
+      currentStyle={initial.styles.results[0]}
+      img={initial.styles.results[0].photos[0].url}
+    />);
+  })
+});
+
+describe('Description feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the Description feature', () => {
+    render(<Description product={initial.product} />);
+  })
+});
+
+describe('Sharing feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the Sharing feature', () => {
+    render(<Sharing />);
+  })
+});
+
+describe('StarsAndReviews feature Test', () => {
+  beforeEach(() => {
+    useState.mockImplementation(jest.requireActual("react").useState);
+  });
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+  test('should render the StarsAndReviews feature', () => {
+    render(<StarsAndReviews reviewScore={helpers.reviewScore(reviewsMeta)} setConfetti={setConfetti} />);
+  })
+});
+
+describe("test the style matrix", () => {
+  test('stylematrix should return an array with a length', () => {
+    expect(helpers.styleMatrix(initial.styles.results, initial.styles.results[0], () => false).length).toBeGreaterThan(0);
+  });
+});
+
+describe("test the form parser", () => {
+  test('form parser should return an object with at least one key', () => {
+    expect(Object.keys(helpers.formParser([{ name: 'name', value: 'value' }])).length).toBeGreaterThan(0);
+  });
+});
+
+describe("test the price checker", () => {
+  test('price checker should return something', () => {
+    expect(helpers.priceChecker(initial.product)).toBeDefined();
+  });
+});
+
+describe("test the review stars function", () => {
+  test('review stars should return something', () => {
+    expect(helpers.reviewStars()).toBeDefined();
+  });
+});
+
+describe("test the review score function", () => {
+  test('review score should return something if there is metadata', () => {
+    expect(helpers.reviewScore(reviewsMeta)).toBeDefined();
+  });
+  test('review score should return null if no data', () => {
+    expect(helpers.reviewScore({ ratings: {} })).toBeNull();
+  });
+});
